4f39cf6a354bb1fb111308fe931d1d8c02b9e7a7
Nathaniel May <nathanieldmay@gmail.com>
2018-10-17 10:59:59 -0400

    added property

--- src/test/scala/com/nathanielmay/quarto/PieceProperties.scala
+++ src/test/scala/com/nathanielmay/quarto/PieceProperties.scala
@@ -3,16 +3,18 @@ package com.nathanielmay.quarto
 //scalacheck
 import org.scalacheck.Properties
 import org.scalacheck.Prop.{BooleanOperators, forAll}
-import testingUtil.Arbitrarily.{aPiece, aTile}
+import testingUtil.Arbitrarily.{aPiece, aColor, aSize, aShape, aTop}
 
 object PieceProperties extends Properties("Piece"){
 
   property("equal to piece with same attributes") = forAll { p: Piece =>
     p == Piece(p.color, p.size, p.shape, p.top)
   }
-  
-//  it should "not be equal to a piece with different attributes" in {
-//    assert(WLQF != Piece(Black, Large, Square, Flat))
-//  }
+
+  property("does not equal piece with different attributes") = forAll {
+    (p: Piece, color: Color, size: Size, shape: Shape, top: Top) =>
+      (p.color != color || p.size != size || p.shape != shape || p.top != top) ==>
+        (p != Piece(color, size, shape, top))
+  }
 
 }
