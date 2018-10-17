af5eca0ae6aa4c65d89c970a5793f5334f87709f
Nathaniel May <nathanieldmay@gmail.com>
2018-10-17 10:42:12 -0400

    added property

--- src/test/scala/com/nathanielmay/quarto/BoardProperties.scala
+++ src/test/scala/com/nathanielmay/quarto/BoardProperties.scala
@@ -10,16 +10,12 @@ import com.nathanielmay.quarto.Exceptions.DuplicatePieceError
 
 object BoardProperties extends Properties("Board"){
 
-  property("decodes reqs") = forAll { (piece: Piece, t1: Tile, t2: Tile) =>
+  property("cannot place a piece twice") = forAll { (piece: Piece, t1: Tile, t2: Tile) =>
     t1 != t2 ==> Board(Map(t1 -> piece, t2 -> piece)).fold(_ == DuplicatePieceError, _ => false)
   }
 
-}
-
-object m {
-  def main(args: Array[String]): Unit = {
-    import testingUtil.Pieces._
-
-    println(Board(Map(Tile(I1, I2) -> WLRH, Tile(I0, I0) -> WLRH)).failed.get)
+  property("cannot occupy a tile twice") = forAll { (p1: Piece, p2: Piece, tile: Tile) =>
+    Board(Map(tile -> p1, tile -> p2)).fold(_ => false, b => b.contains(p2) && b.size == 1)
   }
+
 }
