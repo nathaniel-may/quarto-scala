523a602dbcdb5027b52e28cabde509ed2f92d442
Nathaniel May <nathanieldmay@gmail.com>
2018-10-17 10:35:25 -0400

    corrected and updated scalatest

--- src/test/scala/com/nathanielmay/quarto/BoardTest.scala
+++ src/test/scala/com/nathanielmay/quarto/BoardTest.scala
@@ -1,6 +1,6 @@
 package com.nathanielmay.quarto
 
-import com.nathanielmay.quarto.Exceptions.InvalidPlacementError
+import com.nathanielmay.quarto.Exceptions.DuplicatePieceError
 import org.scalatest.{FlatSpec, Matchers}
 import testingUtil.Pieces.WLQF
 
@@ -8,7 +8,7 @@ class BoardTest extends FlatSpec with Matchers{
 
   "a Quarto board" should "fail if piece is placed twice" in {
     Board(Map(Tile(I1, I2) -> WLQF, Tile(I2, I2) -> WLQF))
-      .failed.get == InvalidPlacementError
+      .fold(_ == DuplicatePieceError, _ => false)
   }
 
 }
