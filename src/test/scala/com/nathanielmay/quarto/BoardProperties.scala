16ed53d073184e8e82f014b8a4edafe663f4d22e
Nathaniel May <nathanieldmay@gmail.com>
2019-07-04 16:38:17 -0400

    removed get

--- src/test/scala/com/nathanielmay/quarto/BoardProperties.scala
+++ src/test/scala/com/nathanielmay/quarto/BoardProperties.scala
@@ -22,13 +22,14 @@ object BoardProperties extends Properties("Board"){
 
   property("placing a piece handles exceptions when not valid") = forAll {
     (p1: Piece, p2: Piece, t1: Tile, t2: Tile) =>
-      Board(Map(t1 -> p1)).get
-        .place(p2, t2) match {
+      Board(Map(t1 -> p1)) map { b =>
+        b.place(p2, t2) match {
           case Success(_)                     => p1 != p2 && t1 != t2
           case Failure(DuplicatePieceError)   => p1 == p2
           case Failure(InvalidPlacementError) => t1 == t2
           case Failure(_)                     => false // unexpected error
         }
+      } fold(_ => false, identity)
   }
 
 }
