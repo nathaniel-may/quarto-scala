0502b298504196d57358115517e122fb2d4caa57
Nathaniel May <nathanieldmay@gmail.com>
2019-07-04 16:36:31 -0400

    added property for place function

--- src/test/scala/com/nathanielmay/quarto/BoardProperties.scala
+++ src/test/scala/com/nathanielmay/quarto/BoardProperties.scala
@@ -1,21 +1,34 @@
 package com.nathanielmay.quarto
 
-//scalacheck
+// Scalacheck
+import Exceptions._
 import org.scalacheck.Properties
 import org.scalacheck.Prop.{BooleanOperators, forAll}
 import testingUtil.Arbs._
 
-//project
+// Scala
+import scala.util.{Success, Failure}
+
+// Project
 import com.nathanielmay.quarto.Exceptions.DuplicatePieceError
 
 object BoardProperties extends Properties("Board"){
 
-  property("cannot place a piece twice") = forAll { (piece: Piece, t1: Tile, t2: Tile) =>
-    t1 != t2 ==> Board(Map(t1 -> piece, t2 -> piece)).fold(_ == DuplicatePieceError, _ => false)
+  property("cannot place a piece twice via apply") = forAll {
+    (piece: Piece, t1: Tile, t2: Tile) => t1 != t2 ==>
+      Board(Map(t1 -> piece, t2 -> piece))
+        .fold(_ == DuplicatePieceError, _ => false)
   }
 
-  property("cannot occupy a tile twice") = forAll { (p1: Piece, p2: Piece, tile: Tile) =>
-    Board(Map(tile -> p1, tile -> p2)).fold(_ => false, b => b.contains(p2) && b.size == 1)
+  property("placing a piece handles exceptions when not valid") = forAll {
+    (p1: Piece, p2: Piece, t1: Tile, t2: Tile) =>
+      Board(Map(t1 -> p1)).get
+        .place(p2, t2) match {
+          case Success(_)                     => p1 != p2 && t1 != t2
+          case Failure(DuplicatePieceError)   => p1 == p2
+          case Failure(InvalidPlacementError) => t1 == t2
+          case Failure(_)                     => false // unexpected error
+        }
   }
 
 }
