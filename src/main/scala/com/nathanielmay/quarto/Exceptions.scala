24be6b881109232e4639cfdfeb74a9013fee9419
Nathaniel May <nathanieldmay@gmail.com>
2019-07-04 14:11:07 -0400

    formatting and sealed class

--- src/main/scala/com/nathanielmay/quarto/Exceptions.scala
+++ src/main/scala/com/nathanielmay/quarto/Exceptions.scala
@@ -1,13 +1,13 @@
 package com.nathanielmay.quarto
 
 object Exceptions {
+  sealed class QuartoError(msg: String) extends Exception(msg)
+  sealed class BoardError(msg: String)  extends QuartoError(msg)
 
-  abstract class QuartoError(msg: String) extends Exception(msg)
-  abstract class BoardError(msg: String) extends QuartoError(msg)
-  case object DuplicatePieceError extends BoardError("each piece may only appear once on a board")
+  case object DuplicatePieceError          extends BoardError("each piece may only appear once on a board")
   case object InvalidPieceForOpponentError extends QuartoError("must choose a valid piece for opponent")
-  case object OutOfTurnError extends QuartoError("not this player's turn")
-  case object InvalidPlacementError extends QuartoError("this board space is already occupied")
-  case object GameOverError extends QuartoError("cannot take a turn on a completed game")
-  case object InvalidPieceForOpponent extends QuartoError("cannot give this piece to opponent")
+  case object OutOfTurnError               extends QuartoError("not this player's turn")
+  case object InvalidPlacementError        extends QuartoError("this board space is already occupied")
+  case object GameOverError                extends QuartoError("cannot take a turn on a completed game")
+  case object InvalidPieceForOpponent      extends QuartoError("cannot give this piece to opponent")
 }
